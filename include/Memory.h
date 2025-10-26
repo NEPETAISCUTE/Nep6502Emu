2c7415aba19b618aa97a569f396e4f858d69b563
NEPETAISCUTE <nepecute@gmail.com>
2025-10-26 14:55:17 +0100

    fixed an issue with GET_WORD that made it go out of bounds

--- include/Memory.h
+++ include/Memory.h
@@ -8,7 +8,7 @@
 // #define SWAP16(data) ((((data) & 0xFF) << 8) | (((data) & 0xFF00) >> 8))
 
 #define MEMORY_GET_BYTE(memory, address) (*(uint8_t*)(((uint8_t*)memory) + ((uint16_t)address)))
-#define MEMORY_GET_WORD(memory, address) (*(uint16_t*)(((uint16_t*)memory) + ((uint16_t)address)))
+#define MEMORY_GET_WORD(memory, address) (*(uint16_t*)(((uint8_t*)memory) + ((uint16_t)address)))
 
 #define MEMORY_SET_BYTE(memory, address, data) ((*(uint8_t*)(((uint8_t*)memory) + ((uint16_t)address))) = ((uint8_t)data))
 #define MEMORY_SET_WORD(memory, address, data) ((*(uint16_t*)(((uint8_t*)memory) + ((uint16_t)address))) = (uint16_t)data)
